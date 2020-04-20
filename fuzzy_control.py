fe10d1f23a7d909dbfd71696b1280ea9ea49b3e7
billmin <pi31415926@163.com>
2020-04-20 18:53:07 +0800

    finished steer membership

--- fuzzy_control.py
+++ fuzzy_control.py
@@ -3,24 +3,28 @@ import numpy as np
 
 
 class FuzzySteer:
-	def __init__(self, curv_domain, steer_domain):
+	def __init__(self):
 		#steer_domain = [-400.0, -200.0, -50.0, -10.0, 0.0, 10.0, 50.0, 200.0, 400.0]
 		self._curv_fuzzy_set = ['CURV_NB+', 'CURV_NB', 'CURV_NS', 'CURV_NS-', 'CURV_ZO', 'CURV_PS-', 'CURV_PS', 'CURV_PB', 'CURV_PB+']
-		self._curv_domain = curv_domain
+		self._curv_domain = [-400.0, -200.0, -50.0, -10.0, 0.0, 10.0, 50.0, 200.0, 400.0]
 		self._steer_fuzzy_set = ['STEER_NB+', 'STEER_NB', 'STEER_NS', 'STEER_NS-', 'STEER_ZO', 'STEER_PS-', 'STEER_PS', 'STEER_PB', 'STEER_PB+']
-		self._steer_domain = steer_domain
+		self._steer_domain = [-4, -3, -2, -1, 0, 1, 2, 3, 4]
 		self._fuzzy_rules = dict()
 		for idx, cf in enumerate(self._curv_fuzzy_set):
 			self._fuzzy_rules[cf] = self._steer_fuzzy_set[idx]
 		
 		self._curv_membership = dict()
 		self._steer_membership = dict()
-		for idx, fs in enumerate(self._curv_fuzzy_set):
-			if fs == 'CURV_NB+' or fs == 'CURV_PB+':
-				self._curv_membership[fs] = 
-			else:
-				self._curv_membership[fs] = 
-
+		# curve's membership
+		self._curv_membership['CURV_NB+'] = np.array([1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+		self._curv_membership['CURV_NB'] = np.array([0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+		self._curv_membership['CURV_NS'] = np.array([0.0, 0.224, 1.0, 0.379, 0.0, 0.0, 0.0, 0.0, 0.0])
+		self._curv_membership['CURV_NS-'] = np.array([0.0, 0.0, 0.71, 1.0, 0.192, 0.0, 0.0, 0.0, 0.0])
+		self._curv_membership['CURV_ZO'] = np.array([0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0])
+		self._curv_membership['CURV_PS-'] = np.array([0.0, 0.0, 0.0, 0.0, 0.192, 1.0, 0.71, 0.0, 0.0])
+		self._curv_membership['CURV_PS'] = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.379, 1.0, 0.224, 0.0])
+		self._curv_membership['CURV_PB'] = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0])
+		self._curv_membership['CURV_PB+'] = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0])
 		# steer's membership
 		self._steer_membership['STEER_NB+'] = np.array([1.0, 0.333, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
 		self._steer_membership['STEER_NB'] = np.array([0.333, 1.0, 0.25, 0.05, 0.0, 0.0, 0.0, 0.0, 0.0])
@@ -67,10 +71,7 @@ class FuzzySteer:
 
 
 if __name__ == '__main__':
-	curv_domain = [-4, -3, -2, -1, 0, 1, 2, 3, 4]
-	steer_domain = [-400.0, -200.0, -50.0, -10.0, 0.0, 10.0, 50.0, 200.0, 400.0]
-	fs = FuzzySteer(curv_domain, steer_domain)
-
+	fs = FuzzySteer()
 	print(fs.curv_fuzzy_set)
 	print(fs.steer_fuzzy_set)
 	print(fs.curv_domain)
