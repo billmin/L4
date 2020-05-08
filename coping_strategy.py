2a6593b5e4384c775431174b0ea2e3b6a2ed05e8
billmin <pi31415926@163.com>
2020-05-08 17:10:20 +0800

    2020-05-08, nothing

--- coping_strategy.py
+++ coping_strategy.py
@@ -2,4 +2,12 @@ import os, sys
 from fuzzy_control import FuzzySteer
 
 
-def orientation_check():
+
+def check_at_origin():
+	# mainly surrounding environment check
+	pass
+
+
+def check_at_destination():
+	# check if it is a proper place for passengers off
+	pass
--- utils.py
+++ utils.py
@@ -7,22 +7,28 @@ class DrvStyle(Enum):
 	NORMAL  = 1
 	RADICAL = 2
 
-def determined_to_execute(driving_style=DrvStyle.NORMAL):
+def patience_for_hindering():
+
+def determined_to_bear_risk(driving_style=DrvStyle.NORMAL, urgent=False):
 	p = random.random()
-	if style == DrvStyle.NORMAL:
-		if 0.0 <= p <= 0.5:
-			return True
+	if not urgent:
+		if style == DrvStyle.NORMAL:
+			if 0.0 <= p <= 0.5:
+				return True
+			else:
+				return False
+		elif style == DrvStyle.MILD:
+			if 0.0 <= p	<= 0.2:
+				return True
+			else:
+				return False
 		else:
-			return False
-	elif style == DrvStyle.MILD:
-		if 0.0 <= p	<= 0.2:
-			return True
-		else:
-			return False
+			if 0.0 <= p <= 0.8:
+				return True
+			else:
+				return False
 	else:
-		if 0.0 <= p <= 0.8:
-			return True
-		else:
-			return False
+		return True
+
 
 
