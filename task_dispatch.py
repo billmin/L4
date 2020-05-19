4c8e9a246441ccb9937b9553d54f35d960540bc8
billmin <pi31415926@163.com>
2020-05-19 21:13:55 +0800

    2020-05-19, task dispatch in progress

--- task_dispatch.py
+++ task_dispatch.py
@@ -1,5 +1,21 @@
 # description of task shifting by non-navigation causes
 
+tasks = ['lane_change_to_left',
+		 'lane_change_to_right',
+		 'turn_left_at_cross',
+		 'turn_right_at_cross',
+		 'go_straight_at_cross',
+		 'u_turn',
+		 'stop_at_obstacle',
+		 'halt_behind_stop_line',
+		 'slow_down_for_pedestrain',
+		 'bypass_obstacle_from_left',
+		 'bypass_obstacle_from_right',
+		 'overtake_from_left',
+		 'overtake_from_right',
+		 'wide_speed_range_adaptive_cruise',
+		 'temporary_stop_by_roadside']
+
 transitions = [
 	{'trigger':'', 'source':'', 'dest':''},
 	{'trigger':'', 'source':'', 'dest':''},
