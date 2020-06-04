15219a5c18124ab77474e54b99db46305d75ae24
billmin <pi31415926@163.com>
2020-06-04 19:07:27 +0800

    2020-06-04

---
+++ control/strategy/multiscenario_strategy.py
@@ -0,0 +1,13 @@
+import os, sys
+from fuzzy_control_reference import *
+
+
+def lane_center_and_adaptive_cruise_control(lateral_deviation_from_lane_center, distance_to_front_vehicle):
+	pass
+
+
+def lane_change_control(lateral_direction_to, ego_speed, distance_to_front_vehicle, front_vehicle_speed):
+	pass
+
+
+
